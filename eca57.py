8d6797cf4dc501cf4c94670809294543bd9d8625
Eli Baum <elimbaum@gmail.com>
2025-03-24 16:37:27 -0400

    arb wires

--- eca57.py
+++ eca57.py
@@ -39,10 +39,13 @@ class Circuit():
 
         return Circuit(gates)
     
-    def perm(self):
+    def perm(self, n=None):
         p = []
-        for i in range(2**self.wires):
-            b = format(i, "0{}b".format(self.wires))
+        if n is None:
+            n = self.wires
+
+        for i in range(2**n):
+            b = format(i, "0{}b".format(n))
             out_list = self.eval(list(map(int, b)))
 
             out_n = reduce(lambda a, b: (a << 1) | b, out_list, 0)
@@ -51,7 +54,7 @@ class Circuit():
         return tuple(p)
     
     def eval(self, input):
-        assert len(input) == self.wires
+        # assert len(input) <= self.wires
 
         state = input[:]
         for g in self.gates:
@@ -97,6 +100,9 @@ class Circuit():
 
         return s
     
+    def __repr__(self):
+        return ";".join(f"{g.a} {g.c[0]} {g.c[1]}" for g in self.gates)
+    
     def __len__(self):
         return len(self.gates)
 
@@ -108,27 +114,57 @@ def no_pairwise_adj(B, m):
 
         yield g
 
+class CircuitRecord():
+    ckt = None
+    count = 0
+
+    def __init__(self, name):
+        self.name = name
+
+    def add(self, ckt):
+        if self.ckt is None:
+            self.ckt = ckt
+            self.count = 1
+        elif len(ckt) < len(self.ckt):
+            self.ckt = ckt
+            self.count = 1
+        elif len(ckt) == len(self.ckt):
+            self.count += 1
+    
+    def __str__(self):
+        return f"{self.name} ({self.count} circuits)"
+
+
 special_perms = {
-    (0, 1, 2, 3, 4, 5, 6, 7): ["Identity", None],
-    (0, 1, 2, 3, 4, 5, 7, 6): ["AND", None],
-    (1, 0, 3, 2, 5, 4, 6, 7): ["NAND", None],
-    (0, 1, 3, 2, 5, 4, 7, 6): ["OR", None],
-    (1, 0, 2, 3, 4, 5, 6, 7): ["NOR", None],
+    (0, 1, 2, 3, 4, 5, 6, 7): CircuitRecord("Identity"),
+    (0, 1, 2, 3, 4, 5, 7, 6): CircuitRecord("AND"),
+    (1, 0, 3, 2, 5, 4, 6, 7): CircuitRecord("NAND"),
+    (0, 1, 3, 2, 5, 4, 7, 6): CircuitRecord("OR"),
+    (1, 0, 2, 3, 4, 5, 6, 7): CircuitRecord("NOR"),
     # (a & !b)
-    (0, 1, 2, 3, 5, 4, 6, 7): ["Implies", None],
+    (0, 1, 2, 3, 5, 4, 6, 7): CircuitRecord("Implies"),
+    # (a & !b)
+    (1, 0, 3, 2, 4, 5, 7, 6): CircuitRecord("NImplies"),
+
+    (0, 1, 3, 2, 5, 4, 6, 7): CircuitRecord("XOR"),
+    (1, 0, 2, 3, 4, 5, 7, 6): CircuitRecord("XNOR"),
+    (1, 0, 3, 2, 5, 4, 7, 6): CircuitRecord("NOT"),
     # (a)
-    (1, 0, 3, 2, 4, 5, 7, 6): ["NImplies", None],
-    (0, 1, 3, 2, 5, 4, 6, 7): ["XOR", None],
-    (1, 0, 2, 3, 4, 5, 7, 6): ["XNOR", None],
-    (1, 0, 3, 2, 5, 4, 7, 6): ["NOT", None],
+    (0, 1, 2, 3, 5, 4, 7, 6): CircuitRecord("CNOT"),
     # (a)
-    (0, 1, 2, 3, 5, 4, 7, 6): ["CNOT", None],
-    # (a)
-    (1, 0, 3, 2, 4, 5, 6, 7): ["NCNOT", None],
+    (1, 0, 3, 2, 4, 5, 6, 7): CircuitRecord("NCNOT"),
+
+    (7, 6, 5, 4, 3, 2, 1, 0): CircuitRecord("Flip"),
 }
 
-def find_ckt(m):
-    B = set(it.permutations([0, 1, 2], 3))
+WIRES = 3
+
+def base_perms(n):
+    return list(it.permutations(range(n), r=3))
+
+def find_ckt(m, B=None):
+    if not B:
+        B = base_perms(WIRES)
     bb = len(B)
 
     perms = Counter()
@@ -139,27 +175,36 @@ def find_ckt(m):
 
     for gl in track(no_pairwise_adj(B, m), total=total):
         ckt = Circuit([ECA(*g) for g in gl])
-        p = ckt.perm()
+        p = ckt.perm(WIRES)
         # if p == tuple(range(8)):
         #     print(ckt)
 
         if p in special_perms:
-            if not special_perms[p][1] or len(ckt) < len(special_perms[p][1]):
-                special_perms[p][1] = ckt
+            special_perms[p].add(ckt)
 
         perms[p] += 1
 
     print(perms.most_common(16)[::-1])
     
     pct = len(perms) / total * 100
-    ppct = len(perms) / (math.factorial(8) // 2) * 100
+    ppct = len(perms) / (math.factorial(2 ** WIRES) // 2) * 100
     print(f"{len(perms)} distinct perms ({pct:.1f}% of circuits, {ppct:.1f}% of perms)")
 
     return perms
 
 q = None
+last_ckt = None
+
+def offset(a, c1, c2):
+    if c1 > a and c1 > c2:
+        return max(a, c2) - c1
+    elif c1 < c2:
+        return min(a, c2) - c1
+    else:
+        return a - c1
 
 def cmd(q):
+    global last_ckt
     if q.startswith("check"):
         m = int(q.split()[1])
         if m < 0:
@@ -179,8 +224,45 @@ def cmd(q):
             find_ckt(m)
     elif q.startswith("special"):
         for p, ckt in special_perms.items():
-            print(f"{ckt[0]} {p}")
-            print(ckt[1])
+            print(f"{ckt} {p}")
+            print(ckt.ckt)
+            print(repr(ckt.ckt))
+    elif q.startswith("wires"):
+        global WIRES
+        split = q.split()
+        if len(split) > 1:
+            WIRES = int(split[1])
+        print(f"n={WIRES}")
+    elif q.startswith("latex"):
+        if not last_ckt:
+            return
+
+        print(last_ckt)
+
+        W = last_ckt.wires
+        wire_store = [[] for _ in range(W)]
+
+        for g in last_ckt.gates:
+            a = g.a
+            c1, c2 = g.c
+
+            for w in range(W):
+
+                if w == a:
+                    wire_store[w].append("\\targ")
+                elif w == c1:
+                    d = offset(a, c1, c2)
+                    wire_store[w].append(f"\\ctrl{{{d}}}")
+                elif w == c2:
+                    d = offset(a, c2, c1)
+                    wire_store[w].append(f"\\ctrlo{{{d}}}")
+                else:
+                    wire_store[w].append("\\qw")
+
+        for w in range(W):
+            s = " & ".join(wire_store[w])
+            print(f"& {s} & \\qw \\\\")
+
     else:
         try:
             ckt = Circuit.from_string(q)
@@ -190,6 +272,7 @@ def cmd(q):
 
         print()
         print(ckt)
+        last_ckt = ckt
         p = ckt.perm()
         for i, j in enumerate(p):
             diffs = j ^ i
